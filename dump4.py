172427819f1528710f7c987a35c6db0bc8fe9c54
Maro8311 <43753598+Maro8311@users.noreply.github.com>
2023-04-12 01:58:25 +0200

    file dumper

---
+++ dump4.py
@@ -0,0 +1,26 @@
+import os
+
+def recurse_directory(path, filelist, allowed_subdirs=None):
+    for root, dirs, files in os.walk(path):
+        relative_root = os.path.relpath(root, path)
+        if allowed_subdirs is not None and not any(relative_root.startswith(subdir) for subdir in allowed_subdirs) and relative_root != '.':
+            continue
+
+        for file in files:
+            if file in filelist:
+                relative_dir = os.path.relpath(root, path)
+                process_file(os.path.join(root, file), relative_dir, file)
+
+def process_file(file_path, relative_dir, filename):
+    with open(file_path, 'r') as file:
+        content = file.read()
+    print(f'/** BEGIN: {relative_dir}/{filename} **/\n')
+    print(content)
+    print(f'/** END: {relative_dir}/{filename} **/\n')
+    print()
+
+if __name__ == '__main__':
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    filelist = ['index.tsx', 'ConnectorFilters.tsx', 'api.ts', 'chargingStations.js', 'chargingStationsApi.ts']
+    allowed_subdirs = ['pages', 'components', 'lib']  # Replace with your list of allowed subdirectories
+    recurse_directory(script_directory, filelist, allowed_subdirs)
